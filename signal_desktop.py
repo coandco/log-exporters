fcb9c82e90c3c72749e39f6a3cc3672c31c5199c
Clint Olson <coandco@gmail.com>
2019-09-05 21:39:35 -0700

    Add the ability to extract attachments as well as text

--- signal_desktop.py
+++ signal_desktop.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import mimetypes
 import argparse
 import json
 import time
@@ -19,12 +20,15 @@ if sys.platform.startswith('win32'):
     APPDATA = os.getenv('APPDATA')
     CONFIG_PATH = os.path.join(APPDATA, 'Signal', 'config.json')
     DB_PATH = os.path.join(APPDATA, 'Signal', 'sql', 'db.sqlite')
+    ATTACHMENT_PATH = os.path.join(APPDATA, 'Signal', 'attachments.noindex')
 elif sys.platform.startswith('darwin'):
     CONFIG_PATH = os.path.expanduser('~/Library/Application Support/Signal/config.json')
     DB_PATH = os.path.expanduser('~/Library/Application Support/Signal/sql/db.sqlite')
+    ATTACHMENT_PATH = os.path.expanduser('~/Library/Application Support/Signal/attachments.noindex')
 else:
     CONFIG_PATH = os.path.expanduser('~/.config/Signal/config.json')
     DB_PATH = os.path.expanduser('~/.config/Signal/sql/db.sqlite')
+    ATTACHMENT_PATH = os.path.expanduser('~/.config/Signal/attachments.noindex')
 
 def dict_factory(cursor, row):
     d = {}
@@ -58,7 +62,7 @@ def make_name(record):
         return str(record["id"])
 
 
-def make_text_log(id_list, outgoing_name, message):
+def make_text_log(id_list, outgoing_name, message, local_attachments=False):
     time_string = time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime(int(message['received_at']) / 1000.0))
 
     message_type = message.get("type", "unknown")
@@ -66,7 +70,7 @@ def make_text_log(id_list, outgoing_name, message):
         name = id_list[int(message["source"])]
         if message["attachments"]:
             attach_message = "[Attachment(s): %s] " % ", ".join(
-                [x["fileName"] or x["contentType"] for x in message["attachments"]])
+                ["%s(%s)" % (x["fileName"] or x["contentType"], x.get("path", "N/A")) for x in message["attachments"]])
         else:
             attach_message = ''
         body = attach_message + demojize(message.get('body') or "")
@@ -74,7 +78,7 @@ def make_text_log(id_list, outgoing_name, message):
         name = outgoing_name
         if message["attachments"]:
             attach_message = "[Attachment(s): %s] " % ", ".join(
-                [x["fileName"] or x["contentType"] for x in message["attachments"]])
+                ["%s(%s)" % (x["fileName"] or x["contentType"], x.get("path", "N/A")) for x in message["attachments"]])
         else:
             attach_message = ''
         body = attach_message + demojize(message.get('body') or "")
@@ -95,23 +99,55 @@ def make_text_log(id_list, outgoing_name, message):
     return unidecode(outstring)
 
 
-def process_convo(cur, id_list, convo_id, outgoing_name, output_dir):
-    filename = "%s.txt" % slugify(demojize(id_list[convo_id]))
+def ensure_dir(dirname):
+    try:
+        os.makedirs(dirname)
+    except OSError:
+        if not os.path.isdir(dirname):
+            raise
+
+def copy_attachments(convo_name, attachment_path, output_dir, message):
+    atch_out_dir = os.path.join(output_dir, convo_name)
+    ensure_dir(atch_out_dir)
+    for atch in message.get("attachments", []):
+        # If we don't have a local copy of the file, we're done.
+        if "path" not in atch.keys():
+            continue
+
+        if atch["fileName"]:
+            filename = atch["fileName"]
+        else:
+            extension = mimetypes.guess_extension(atch["contentType"])
+            if not extension:
+                extension = "." + atch["contentType"].split("/")[-1]
+            if extension == ".jpe":
+                extension = ".jpg"
+            filename = "%s%s" % (atch["id"], extension)
+        src = os.path.join(attachment_path, atch["path"])
+        dest = os.path.join(atch_out_dir, filename)
+        shutil.copy2(src, dest)
+
+
+def process_convo(cur, id_list, convo_id, outgoing_name, process_attachments, attachment_path, output_dir):
+    convo_name = slugify(demojize(id_list[convo_id]))
+    filename = "%s.txt" % convo_name
     with open(os.path.join(output_dir, filename), "w") as outfile:
         cur.execute("select json from messages where conversationId = ? order by sent_at asc", [convo_id])
         convo_objs = [json.loads(x["json"]) for x in cur.fetchall()]
         for message in convo_objs:
-            line = make_text_log(id_list, outgoing_name, message)
+            line = make_text_log(id_list, outgoing_name, message, local_attachments=process_attachments)
             if line:
                 outfile.write(line + "\n")
+            if process_attachments:
+                copy_attachments(convo_name, attachment_path, output_dir, message)
 
 
-def main(key, db_path, outgoing_name, output_dir):
+def main(key, db_path, outgoing_name, process_attachments, attachment_path, output_dir):
     with open_db(key, db_path) as cur:
         cur.execute("select * from conversations;")
         id_list = {x["id"]: make_name(x) for x in cur.fetchall()}
         for convo_id in id_list.keys():
-            process_convo(cur, id_list, convo_id, outgoing_name, output_dir)
+            process_convo(cur, id_list, convo_id, outgoing_name, process_attachments, attachment_path, output_dir)
 
 
 if __name__ == "__main__":
@@ -122,6 +158,8 @@ if __name__ == "__main__":
     key_methods.add_argument('-k', '--key', help="Decryption key for Signal Desktop database")
     parser.add_argument('-d', '--db-path', default=DB_PATH, help="Location of the encrypted Signal sqlite.db file")
     parser.add_argument('-i', '--i-am', default="me", help="Name to tag outgoing messages with")
+    parser.add_argument('--extract-attachments', action="store_true", help="Extract attachments as well as text")
+    parser.add_argument('--attachment-path', default=ATTACHMENT_PATH, help="Location of the attachments to extract")
     parser.add_argument('output_dir', help="Directory to output log files to")
 
     args = parser.parse_args()
@@ -131,4 +169,4 @@ if __name__ == "__main__":
         with open(args.json, 'r') as config:
             resolved_key = json.load(config).get("key")
 
-    main(resolved_key, args.db_path, args.i_am, args.output_dir)
+    main(resolved_key, args.db_path, args.i_am, args.extract_attachments, args.attachment_path, args.output_dir)
