c6483dc7a52b3f0b2ffd3cd74425ba235fcb1ff4
Clint Olson <coandco@gmail.com>
2023-09-26 16:19:18 -0700

    Switch to date-based log files and make them resumable

--- signal_desktop.py
+++ signal_desktop.py
@@ -8,12 +8,17 @@ import sqlite3
 import shutil
 import json
 import time
-import os
 import sys
+import os
+import re
 
 from distutils.spawn import find_executable
 from contextlib import contextmanager
 from hashlib import md5
+from pathlib import Path
+from datetime import datetime
+from typing import Iterable
+from packaging import version
 
 from unidecode import unidecode
 from slugify import slugify
@@ -76,6 +81,35 @@ def open_db(key, db_path):
             os.unlink(tmpfilename)
 
 
+# Adapted from https://stackoverflow.com/a/54278929
+def read_lines_from_end(filename: Path) -> Iterable[str]:
+    with filename.open('rb') as f:
+        try:  # catch OSError in case of a one line file
+            f.seek(-2, os.SEEK_END)
+            while True:
+                while f.read(1) != b'\n':
+                    f.seek(-2, os.SEEK_CUR)
+                curpos = f.tell()
+                yield f.readline().decode()
+                # Jump back to just before the newline we read
+                f.seek(curpos-2)
+        except OSError:
+            f.seek(0)
+            yield f.readline().decode()
+
+
+TIMESTAMP_PATTERN = re.compile(r'^\[(?P<timestamp>[0-9: -]*)]')
+
+
+def read_last_timestamp(filename: Path) -> float:
+    for line in read_lines_from_end(filename):
+        match = TIMESTAMP_PATTERN.search(line)
+        if match:
+            return datetime.strptime(match.group('timestamp'), "%Y-%m-%d %H:%M:%S").timestamp()
+    # If we didn't find any timestamp, error
+    raise ValueError(f"No timestamp found in file {filename}")
+
+
 def make_name(record):
     name = str(record["id"])
     if record["name"]:
@@ -89,8 +123,9 @@ def make_name(record):
     return name.translate({ord(c): None for c in chars_to_remove})
 
 
-def make_text_log(id_list, outgoing_name, message, local_attachments=False):
-    time_string = time.strftime('[%Y-%m-%d %H:%M:%S]', time.localtime(int(message['received_at']) / 1000.0))
+def make_text_log(id_list, outgoing_name, message):
+    message_time = time.localtime(int(message.get('received_at_ms', message['received_at'])) / 1000.0)
+    time_string = time.strftime('[%Y-%m-%d %H:%M:%S]', message_time)
 
     message_type = message.get("type", "unknown")
     if message_type == "incoming":
@@ -111,7 +146,7 @@ def make_text_log(id_list, outgoing_name, message, local_attachments=False):
             attach_message = ''
         body = attach_message + demojize(message.get('body') or "")
     elif message_type == "keychange":
-        name = id_list.get(message["key_changed"], "Unknown")
+        name = id_list.get(message.get("key_changed", ''), "Unknown")
         body = "[Safety number changed]"
     elif message_type == "verified-change":
         name = id_list.get(message["verifiedChanged"], "Unknown")
@@ -134,9 +169,10 @@ def ensure_dir(dirname):
         if not os.path.isdir(dirname):
             raise
 
-def copy_attachments(convo_name, attachment_path, output_dir, message):
-    atch_out_dir = os.path.join(output_dir, convo_name)
-    ensure_dir(atch_out_dir)
+
+def copy_attachments(attachment_path, output_dir, message):
+    atch_out_dir = Path(output_dir) / "attachments"
+    atch_out_dir.mkdir(parents=True, exist_ok=True)
     for atch in message.get("attachments", []):
         # If we don't have a local copy of the file, we're done.
         if "path" not in atch.keys():
@@ -153,25 +189,50 @@ def copy_attachments(convo_name, attachment_path, output_dir, message):
             identifier = (atch.get("attachment_identifier", None) or
                           atch.get("id", None) or
                           atch.get("cdnKey", None) or
-                          md5(str(atch)).hexdigest())
+                          md5(str(atch).encode('utf-8')).hexdigest())
             filename = "%s%s" % (identifier, extension)
-        src = os.path.join(attachment_path, atch["path"])
-        dest = os.path.join(atch_out_dir, filename)
+        src = Path(attachment_path) / atch["path"]
+        dest = Path(atch_out_dir) / filename
         shutil.copy2(src, dest)
 
 
-def process_convo(cur, id_list, convo_map, convo_id, outgoing_name, process_attachments, attachment_path, output_dir):
+def process_convo(cur, id_list, convo_map, convo_id, outgoing_name, process_attachments, attachment_path, base_output_dir):
     convo_name = slugify(demojize(convo_map[convo_id]))
-    filename = "%s.txt" % convo_name
-    with open(os.path.join(output_dir, filename), "w") as outfile:
-        cur.execute("select json from messages where conversationId = ? order by sent_at asc", [convo_id])
-        convo_objs = [json.loads(x["json"]) for x in cur.fetchall()]
-        for message in convo_objs:
-            line = make_text_log(id_list, outgoing_name, message, local_attachments=process_attachments)
-            if line:
-                outfile.write(line + "\n")
-            if process_attachments:
-                copy_attachments(convo_name, attachment_path, output_dir, message)
+    cur.execute("select json from messages where conversationId = ? order by sent_at asc", [convo_id])
+    convo_objs = [json.loads(x["json"]) for x in cur.fetchall()]
+    current_file = None
+    current_month = None
+    current_time = 0
+    output_dir = Path(base_output_dir) / convo_name
+    output_dir.mkdir(parents=True, exist_ok=True)
+    for message in convo_objs:
+        message_timestamp = int(message.get('received_at_ms', message['received_at'])) / 1000.0
+        time_string = time.strftime('%Y_%m', time.localtime(message_timestamp))
+        if time_string != current_month:
+            if current_file:
+                current_file.close()
+            out_filename = output_dir / f"{convo_name}_{time_string}.txt"
+            if out_filename.exists():
+                try:
+                    current_time = read_last_timestamp(out_filename)
+                except ValueError:
+                    # Only update our current time if we've successfully parsed the last time
+                    print(f"WARNING: no timestamp found for pre-existing file {out_filename}")
+                    pass
+            current_file = out_filename.open("a")
+            current_month = time_string
+
+        # Adding one second here so we don't repeat the last line of the file when rerunning
+        if message_timestamp < (current_time + 1.0):
+            continue
+
+        line = make_text_log(id_list, outgoing_name, message)
+        if line:
+            current_file.write(line + "\n")
+        if process_attachments:
+            copy_attachments(attachment_path, output_dir, message)
+    if current_file:
+        current_file.close()
 
 
 def main(key, db_path, outgoing_name, process_attachments, attachment_path, output_dir):
@@ -179,7 +240,7 @@ def main(key, db_path, outgoing_name, process_attachments, attachment_path, outp
         cur.execute("select * from conversations;")
         raw_conversations = cur.fetchall()
         id_list = {x["e164"]: make_name(x) for x in raw_conversations if x["e164"] is not None}
-        id_list.update({x["uuid"]: make_name(x) for x in raw_conversations if x["uuid"] is not None})
+        id_list.update({x["uuid"]: make_name(x) for x in raw_conversations if x.get("uuid", None)})
         convo_map = {x["id"]: make_name(x) for x in raw_conversations}
         for convo_id in convo_map.keys():
             process_convo(cur, id_list, convo_map, convo_id, outgoing_name, process_attachments, attachment_path, output_dir)
@@ -206,5 +267,13 @@ if __name__ == "__main__":
 
     if not find_executable('sqlcipher'):
         parser.error("sqlcipher must be on your path for this script to function")
+
+    if version.parse(sqlite3.sqlite_version) < version.parse('3.43.1'):
+        howto_url = "https://shuaib.org/technical-guide/how-to-update-or-upgrade-sqlite3-version-in-python/"
+        download_url = "https://www.sqlite.org/download.html"
+        print("Signal uses a more recent version of sqlite than you have installed")
+        print(f"To update your version, see {howto_url} for linux/macos,")
+        print(f"or just download the windows .dll from {download_url} and replace sqlite.dll in the DLLs folder of your Python installation")
+        parser.error("sqlite3 version is too old (<3.43.1)")
 
     main(resolved_key, args.db_path, args.i_am, args.extract_attachments, args.attachment_path, args.output_dir)
